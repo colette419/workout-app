1145073414d51cdeee75cd8e90c66d927ff96568
Colette <colette419@gmail.com>
2016-07-21 12:46:04 -0400

    first commit

--- src/app/workout-app.component.ts
+++ src/app/workout-app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { ZipcodeAreaComponent } from './zipcode-area/zipcode-area.component';
 
 @Component({
   moduleId: module.id,
   selector: 'workout-app-app',
-  templateUrl: 'workout-app.component.html',
-  styleUrls: ['workout-app.component.css']
+  template: `
+  <h1>How's the Workout Weather?</h1>
+  <app-zipcode-area></app-zipcode-area>
+  `,
+  styleUrls: ['workout-app.component.css'],
+  directives: [ZipcodeAreaComponent]
 })
 export class WorkoutAppAppComponent {
   title = 'workout-app works!';
---
+++ src/app/zipcode-area/zipcode-area.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-zipcode-area',
+  templateUrl: 'zipcode-area.component.html',
+  styleUrls: ['zipcode-area.component.css']
+})
+export class ZipcodeAreaComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+}
